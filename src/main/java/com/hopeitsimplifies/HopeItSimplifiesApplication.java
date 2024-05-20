4e22d41085414dcb792bb1327e7e282997590842
TuanThanh0611 <nguyenhatuanthanh1901@gmail.com>
2024-05-20 21:53:19 +0700

    Ivo

---
+++ src/main/java/com/hopeitsimplifies/HopeItSimplifiesApplication.java
@@ -0,0 +1,13 @@
+package com.hopeitsimplifies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeItSimplifiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopeItSimplifiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hopeitsimplifies/HopeItSimplifiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopeitsimplifies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeItSimplifiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
